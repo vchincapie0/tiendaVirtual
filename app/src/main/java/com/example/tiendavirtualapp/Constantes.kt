eaa5faf8233aae7f824532222898a3b181f513a5
Vivian Hincapie <carohinca1997@gmail.com>
2025-11-05 21:18:05 -0500

    Se añade constante para que los usuarios puedan seleccionar favoritos

--- app/src/main/java/com/example/tiendavirtualapp/Constantes.kt
+++ app/src/main/java/com/example/tiendavirtualapp/Constantes.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.text.format.DateFormat
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-
+import com.google.firebase.database.FirebaseDatabase
 import java.util.Calendar
 import java.util.Locale
 
@@ -20,6 +20,42 @@ class Constantes {
         calendar.timeInMillis = tiempo
 
         return DateFormat.format("dd/MM/yyyy",calendar).toString()
+    }
+
+
+    /*Agregar producto a la base de datos*/
+    fun agregarProductoFav(context : Context , idProducto : String){
+        val firebaseAuth = FirebaseAuth.getInstance()
+        val tiempo = Constantes().obtenerTiempoD()
+
+        val hashMap = HashMap<String , Any> ()
+        hashMap["idProducto"] = idProducto
+        hashMap["idFav"] = tiempo
+
+        val ref = FirebaseDatabase.getInstance().getReference("Usuarios")
+        ref.child(firebaseAuth.uid!!).child("Favoritos").child(idProducto)
+            .setValue(hashMap)
+            .addOnSuccessListener {
+                Toast.makeText(context, "Producto agregado a favoritos", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener { e->
+                Toast.makeText(context, "${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    /*Eliminar producto a la base de datos*/
+    fun eliminarProductoFav(context: Context, idProducto: String){
+        var firebaseAuth = FirebaseAuth.getInstance()
+
+        val ref = FirebaseDatabase.getInstance().getReference("Usuarios")
+        ref.child(firebaseAuth.uid!!).child("Favoritos").child(idProducto)
+            .removeValue()
+            .addOnSuccessListener {
+                Toast.makeText(context, "Producto eliminado de favoritos", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {e->
+                Toast.makeText(context, "${e.message}", Toast.LENGTH_SHORT).show()
+            }
     }
 
 }
