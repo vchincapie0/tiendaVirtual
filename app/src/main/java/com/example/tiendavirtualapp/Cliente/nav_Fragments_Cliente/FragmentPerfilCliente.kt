264f1dcbb5d45365329de490953f282df660a7f3
Vivian Hincapie <carohinca1997@gmail.com>
2025-11-07 22:33:03 -0500

    Seccion perfil del cliente

--- app/src/main/java/com/example/tiendavirtualapp/Cliente/nav_Fragments_Cliente/FragmentPerfilCliente.kt
+++ app/src/main/java/com/example/tiendavirtualapp/Cliente/nav_Fragments_Cliente/FragmentPerfilCliente.kt
@@ -5,18 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.tiendavirtualapp.Cliente.Bottom_Nav_Fragments_Cliente.FragmentCarritoC
+import com.example.tiendavirtualapp.Cliente.Bottom_Nav_Fragments_Cliente.FragmentFavoritosC
+import com.example.tiendavirtualapp.Cliente.Bottom_Nav_Fragments_Cliente.FragmentMisOrdenesC
+import com.example.tiendavirtualapp.Cliente.Bottom_Nav_Fragments_Cliente.FragmentTiendaCliente
 import com.example.tiendavirtualapp.R
+import com.example.tiendavirtualapp.databinding.FragmentInicioCBinding
 
 
 class FragmentPerfilCliente : Fragment() {
 
+    private lateinit var binding : FragmentInicioCBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil_cliente, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentInicioCBinding.inflate(inflater,container, false)
+
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.op_tienda_c->{
+                    replaceFragment(FragmentTiendaCliente())
+                }
+                R.id.op_favoritos_c->{
+                    replaceFragment(FragmentFavoritosC())
+                }
+                R.id.op_carrito_c->{
+                    replaceFragment(FragmentCarritoC())
+                }
+                R.id.op_mis_ordenes_c->{
+                    replaceFragment(FragmentMisOrdenesC())
+                }
+            }
+            true
+        }
+
+        replaceFragment(FragmentTiendaCliente())
+        binding.bottomNavigation.selectedItemId = R.id.op_tienda_c
+
+        return binding.root
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        parentFragmentManager
+            .beginTransaction()
+            .replace(R.id.bottomFragment,fragment)
+            .commit()
     }
 
 
